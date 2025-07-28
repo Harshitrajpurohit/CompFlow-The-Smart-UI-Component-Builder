5d61aeb1a7ae618d2097036a1775d5ac2833377f
Harshitrajpurohit <indianhars10@gmail.com>
2025-07-29 00:57:00 +0530

    mongo url edit

--- backend/models/connectDB.js
+++ backend/models/connectDB.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 async function ConnectDB() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/compflow");
+    await mongoose.connect(process.env.MONGODB_KEY);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
